eb2fa2a1e233740efab0d61f127771b6ddb950ce
CJ <codecraftcj@gmail.com>
2024-02-23 10:54:34 +0000

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  build: {
+    // Customize the build output
+    outDir: 'dist',
+    assetsDir: '.', // keep the original directory structure
+    emptyOutDir: false, // do not delete the output directory before rebuilding
+
+    // Copy specific files to the output directory
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // disable code splitting
+      },
+    },
+  },
 })
